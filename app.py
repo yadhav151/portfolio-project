08eae7fe6cc20664ec3b14e4aaecb8b702a9ef90
Yadhav C <yadhavashokan151@gmail.com>
2026-02-26 19:28:14 +0530

    Corrected Railway MySQL environment variables

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 # MySQL connection
 db = mysql.connector.connect(
-    host=os.getenv("mysql.railway.internal"),
-    user=os.getenv("root"),
-    password=os.getenv("OtmzInrhblxgtvKrmEKijUGvxPZeJqaW"),
-    database=os.getenv("railway"),
-    port=int(os.getenv("3306"))
+    host=os.getenv("MYSQLHOST"),
+    user=os.getenv("MYSQLUSER"),
+    password=os.getenv("MYSQLPASSWORD"),
+    database=os.getenv("MYSQLDATABASE"),
+    port=int(os.getenv("MYSQLPORT", 3306))
 )
 @app.route("/")
 def home():
